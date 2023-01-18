285d387c3c04e61c023981c1f556c68102593460
antalandreiu <antalandreiu@gmail.com>
2023-01-18 12:00:44 +0100

    api done

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,4 @@
 import joblib
-import pandas as pd
-from dateutil.relativedelta import relativedelta
-import model
 from fastapi import FastAPI
 from datetime import datetime
 
@@ -14,18 +11,18 @@ PREDICTED_YEARS = 60
 app = FastAPI()
 
 
-@app.get("/getForecast/{region}/{province}/{type_of_exercise}/{tourist_residence}")
+@app.get("/{region}/{province}/{type_of_exercise}/{tourist_residence}")
 def forecast(region: str, province: str, type_of_exercise: str, tourist_residence: str):
+
     path = f"./../data/{region}/models/{province}/{type_of_exercise}/{tourist_residence}"
-    #df = pd.read_csv(f"{path}/df.csv")
     with open(f"{path}/arrivals_model.pkl", "rb") as model:
         forecaster = joblib.load(model)
         prediction = forecaster.forecast(PREDICTED_YEARS)
 
-    prediction = prediction.to_dict()
-    values = list(prediction.values())
-    indexes = list(prediction.keys())
-    #indexes = [END_TRAIN + relativedelta(months=x) for x in range(PREDICTED_YEARS)]
+    values = list(prediction.array)
+    indexes = list(prediction.index)
+    assert len(values) == len(indexes)
 
-    # df.to_dict(orient="records")
-    return{"prediction": values, "indexes": indexes}
+    data = [{"date": indexes[i],  "npeople": values[i]} for i in range(len(values))]
+
+    return {"prediction": data}
